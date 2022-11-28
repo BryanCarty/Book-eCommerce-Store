c955e1d3a5af226a7144671ef8a02923e10718da
Dennybah <Dennybah69@gmail.com>
2022-11-28 16:00:35 +0000

    Program that pulls info from sql server table

---
+++ BlazorApp2/BlazorApp2/Server/Interfaces/IUser.cs
@@ -0,0 +1,13 @@
+﻿using BlazorApp2.Shared.Models;
+
+namespace BlazorApp2.Server.Interfaces
+{
+    public interface IUser
+    {
+        public List<User> GetUserDetails();
+        public void AddUser(User user);
+        public void UpdateUserDetails(User user);
+        public User GetUserData(int id);
+        public void DeleteUser(int id);
+    }
+}
